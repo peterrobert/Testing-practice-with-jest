0a87c4b7900542c39c1820fd49150f766db1b812
peterrobert <pwambui93@gmail.com>
2020-10-13 12:37:22 +0300

    capitalize the string tested

--- app.test.js
+++ app.test.js
@@ -24,7 +24,16 @@ test("Divides the values given", () =>{
 
 test("The value should be a string", () => {
 
-    let data = "this is a test case"
-  expect (capitalize.capitalizeString.check(data)).toBe('string');
+    let data = "this is a test case";
+    expect(capitalize.capitalizeString(data).check).toBe('string')
+  
+});
+
+
+test("the first letter should be capitalized", () => {
+    
+    let data = "capitalize this string";
+    expect(capitalize.capitalizeIt(data).uppercase).toEqual(true);
+
 })
 
--- capitalize.js
+++ capitalize.js
@@ -1,17 +1,35 @@
 const capitalizeString = (value) => {
-   value = "this is the value";
+  value = "hey there as tested";
+  let check = typeof value;
 
-   let check = (v) => {
-       v = value
-      return typeof(v)
-   }
-  
-
-
-
-    return {check}
+  return {
+    check,
+    value,
+  };
 };
 
+const capitalizeIt = () => {
+  let uppercase = false;
 
+  if (capitalizeString().check == "string") {
+    let newValue = capitalizeString().value.slice(0, 1).toUpperCase();
+    let rest = capitalizeString().value.slice(1);
+    let capitalizedAns = newValue + rest;
 
-module.exports = {capitalizeString};
+    if (capitalizedAns.slice(0, 1).toUpperCase()) {
+      uppercase = true;
+    } else {
+      uppercase = false;
+    }
+
+    return {
+      capitalizedAns,
+      uppercase,
+    };
+  }
+};
+
+module.exports = {
+  capitalizeString,
+  capitalizeIt,
+};
